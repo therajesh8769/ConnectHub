087d55083d8781ee0935877664d0c46d1f3d0d08
Rajesh <therajesh8769@gmail.com>
2025-08-02 21:55:02 +0530

    added vercel.json

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5001/api';
+const API_BASE_URL = 'https://connecthub-blue.vercel.app/api';
 
 // Create axios instance
 const api = axios.create({
